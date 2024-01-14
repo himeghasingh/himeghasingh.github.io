1dfdfa422f97bd19fabb2d7db41436e62b26ad41
himeghasingh <meghamukeshsingh@outlook.com>
2024-01-14 20:27:59 +0530

    Adding hamburger icon functionality

--- megha-portfolio/src/components/NavBar.jsx
+++ megha-portfolio/src/components/NavBar.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {FaBars, FaTimes} from "react-icons/fa";
 
 const NavBar = () => {
+    
+    const [nav, setNav] = useState(false);
+
     const links = [
         {
             id : 1,
@@ -24,20 +27,31 @@ const NavBar = () => {
             link : 'CONTACT'
         },
     ]
+
     return (
     <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed">
         <div>
-            <h1 className="font-bold font-secondary ml-2 hover:scale-110 duration-200 cursor-pointer">MEGHA.</h1>
+            <h1 className="font-bold font-secondary ml-2 hover:scale-105 duration-200 cursor-pointer">MEGHA.</h1>
         </div>
 
-        <ul className="flex">
+        <ul className="hidden md:flex">
             {links.map(({id, link}) => (
-                <li key = {id} className="px-4 cursor-pointer capitalize font-secondary text-gray-500 hover:scale-110 duration-200 text-sm font-semibold">
+                <li key = {id} className="px-4 cursor-pointer capitalize font-secondary text-gray-500 hover:scale-105 duration-200 text-sm font-semibold">
                 {link}
             </li>
             ))}
-            
         </ul>
+        <div onClick = {() => setNav(!nav)} className="cursor-pointer pr-4 z-10 md:hidden">
+            {nav ? <FaTimes  size={20} /> : <FaBars size={20} />}
+        </div>
+        {nav && (<ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-700 text-white">
+        {links.map(({id, link}) => (
+                <li key = {id} className="py-4 cursor-pointer capitalize font-secondary text-white hover:scale-105 duration-200 text-sm font-semibold">
+                {link}
+            </li>
+            ))}
+        </ul>
+        )}
     </div>
   )
 }
