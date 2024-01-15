8ee8b831f95b622433beea6384b4aeff49fd1cab
himeghasingh <meghamukeshsingh@outlook.com>
2024-01-15 07:25:29 +0530

    Added Router links for all navbar components

--- megha-portfolio/src/components/NavBar.jsx
+++ megha-portfolio/src/components/NavBar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import {FaBars, FaTimes} from "react-icons/fa";
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
     
@@ -8,18 +9,22 @@ const NavBar = () => {
     const links = [
         {
             id : 1,
+            path : '/about',
             link : 'ABOUT'
         },
         {
             id : 2,
+            path : 'education',
             link : 'EDUCATION'
         },
         {
             id : 3,
+            path : 'experience',
             link : 'EXPERIENCE'
         },
         {
             id : 4,
+            path : 'projects',
             link : 'PROJECTS'
         },
         // {
@@ -31,13 +36,15 @@ const NavBar = () => {
     return (
     <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed">
         <div>
+            <Link to="/">
             <h1 className="font-bold font-secondary ml-2 hover:scale-105 duration-200 cursor-pointer">MEGHA.</h1>
+            </Link>
         </div>
 
         <ul className="hidden md:flex">
-            {links.map(({id, link}) => (
+            {links.map(({id, path, link}) => (
                 <li key = {id} className="px-4 cursor-pointer capitalize font-secondary text-gray-500 hover:scale-105 duration-200 text-sm font-semibold hover:text-gray-100">
-                {link}
+                <Link to={path}>{link}</Link>
             </li>
             ))}
         </ul>
@@ -45,9 +52,9 @@ const NavBar = () => {
             {nav ? <FaTimes  size={20} /> : <FaBars size={20} />}
         </div>
         {nav && (<ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-700 text-white">
-        {links.map(({id, link}) => (
+        {links.map(({id, path, link}) => (
                 <li key = {id} className="py-4 cursor-pointer capitalize font-secondary text-white hover:scale-105 duration-200 text-sm font-semibold">
-                {link}
+                <Link to={path}>{link}</Link>
             </li>
             ))}
         </ul>
